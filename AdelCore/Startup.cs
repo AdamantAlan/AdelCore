6d5eb706d5114c1031a9cd936658d524ef6c13bd
AdamantAlan <alan.dark1137@gmail.com>
2021-02-16 22:22:19 +0500

    test commit

--- AdelCore/Startup.cs
+++ AdelCore/Startup.cs
@@ -44,6 +44,7 @@ namespace AdelCore
 
             app.UseEndpoints(endpoints =>
             {
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
